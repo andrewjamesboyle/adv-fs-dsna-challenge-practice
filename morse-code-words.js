614a8887e2a2d8ab9be3b2c3c185f5c7c6d32cea
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2023-01-27 15:19:56 -0800

    rough draft of morse code problem solution

---
+++ morse-code-words.js
@@ -0,0 +1,13 @@
+function uniqueWords(words) {
+    const codes = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."];
+    let transformedWords = []
+
+    for (i = 0; i < words.length; i++) {
+        const transformedWord = (some relationship between words letter index number and corresponding codes[i]);
+            
+        transformedWords.append(transformedWord)
+        }
+    }
+
+    return transformedWords.length;
+};
