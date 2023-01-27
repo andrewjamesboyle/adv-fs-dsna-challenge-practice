03345e4c9eec6bc51897a2bb63414be4d6561f14
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2023-01-27 09:03:08 -0800

    convenience store planning

---
+++ convenience-store.js
@@ -0,0 +1,11 @@
+function changeEnough(coins, amount) {
+    let coins = []
+    let amount = 0
+
+    const quarters = coins[0] * 25;
+    const dimes = coins[1] * 10;
+    const nickels = coins[2] * 5;
+    const pennies = coins[3] * 1;
+
+    
+}
