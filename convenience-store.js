8e3753141b7b931a26907cb3f35977493488d19c
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2023-01-27 10:22:59 -0800

    solve convenience store challenge

--- convenience-store.js
+++ convenience-store.js
@@ -1,11 +1,43 @@
 function changeEnough(coins, amount) {
-    let coins = []
-    let amount = 0
+    
+    let counter = 0
 
-    const quarters = coins[0] * 25;
-    const dimes = coins[1] * 10;
-    const nickels = coins[2] * 5;
-    const pennies = coins[3] * 1;
+    for (let i = 0; i < coins.length; i++ ) {
+        if (i === 0) {
+            counter += coins[i] * 25
+        } else if (i === 1) {
+            counter += coins[i] * 10
+        } else if (i === 2) {
+            counter += coins[i] * 5
+        } else if (i === 3) {
+            counter += coins[i]
+        }
+    }
+    
+    return counter >= amount * 100
 
+}
+
+
+function changeEnoughReduce(coins, amount) {
+    
+    const total = coins.reduce((acc, currentValue, currentIndex) => {
+        
+        if (currentIndex === 0) {
+            acc += currentValue * 25
+        } else if (currentIndex === 1) {
+            acc += currentValue * 10
+        } else if (currentIndex === 2) {
+            acc += currentValue * 5
+        } else if (currentIndex === 3) {
+            acc += currentValue
+        }
+        
+        return acc
+    }, 0)
+    
+    return total >= amount * 100
     
 }
+
+console.log(changeEnoughReduce([4, 0, 0, 7], 4.50));
